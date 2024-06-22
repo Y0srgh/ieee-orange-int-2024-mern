17c74d65cbb4ea9ae75b6de36777836519ae7d27
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-06-22 10:27:13 +0100

    added an employee model

---
+++ backend/models/employeeModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const employeeSchema = mongoose.Schema(
+  {
+    nom: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Employee = mongoose.model("Employee", employeeSchema);
